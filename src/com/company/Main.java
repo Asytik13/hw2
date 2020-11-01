4bbe1cd0fc1e8c8ecd061fad4dde09fecb623b9a
olga <pakhomovaolga96@gmail.com>
2020-11-01 19:54:32 +0100

    - added unit test;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,33 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+        int shift = 5;
+
+        String message = "uzgqnh%xyfynh%{tni%rfns-Xywnsl`b%fwlx.";
+        // System.out.println(Main.encrypt(shift, message));
+        System.out.println(Main.decrypt(shift, message));
+    }
+
+    public static String encrypt(int shift, String message){
+        char[] result = new char[message.length()];
+        for ( int i = 0 ; i < message.length() ; i++ ) {
+            result[i] = (char) ( message.charAt(i) + shift );
+        }
+        return new String(result);
+    }
+
+    public static String decrypt(int shift, String message){
+        char[] result = new char[message.length()];
+        for ( int i = 0 ; i < message.length() ; i++ ) {
+            result[i] = (char) ( message.charAt(i) - shift );
+        }
+        return new String(result);
     }
 }
+
+
+
+
+
+
---
+++ src/test/CryptographyTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import com.company.Main;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
+public class CryptographyTest {
+    @Test
+    public void encrypt_abc_fgh(){
+        String expected = "fgh";
+        String actual = Main.encrypt(5, "abc");
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void decrypt_fgh_abc(){
+        String expected = "abc";
+        String actual = Main.decrypt(5, "fgh");
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void encrypt_null_nullPointerException(){
+        assertThatNullPointerException().isThrownBy(() -> {
+            Main.encrypt(5, null);
+        });
+    }
+}
