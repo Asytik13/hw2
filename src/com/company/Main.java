5f7be460352007b8ff6ce11cdc441bb0932a170a
olga <pakhomovaolga96@gmail.com>
2020-11-01 20:27:06 +0100

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+        System.out.println(Main.factorial(5));
+    }
+
+    private static int factorial(int num){
+        return num * Main.factorial(num - 1);
     }
 }
