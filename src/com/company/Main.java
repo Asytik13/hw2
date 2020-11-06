926d9ecf1bbcf75aa52c95f19f22b1c59f930866
olga <pakhomovaolga96@gmail.com>
2020-11-06 20:50:48 +0100

    - added solution by stream rangClosed();

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,24 @@
 package com.company;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println(Main.factorial(5));
+        int num = 5;
+        System.out.println(Main.factorial(num));
+        System.out.println(Main.factorialStream(num));
     }
 
     private static int factorial(int num){
+        if ( num <= 2 ) {
+            return num;
+        }
         return num * Main.factorial(num - 1);
+    }
+
+    private static int factorialStream(int num){
+        int result = IntStream.rangeClosed(1, num).reduce(1, ( (x, y) -> x * y ));
+        return result;
     }
 }
