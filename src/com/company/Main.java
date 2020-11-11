05371b44ea7ff77419a868514163e334795319ee
olga <pakhomovaolga96@gmail.com>
2020-11-11 13:52:59 +0100

    - added factorial for big numbers method; - added fibonacci()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,20 @@
 package com.company;
 
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args){
-        int num = 5;
-        System.out.println(Main.factorial(num));
-        System.out.println(Main.factorialStream(num));
+        // Main.factorialBigNumbers(5);
+        // Main.calculateFactorial(25);
+        int[] a = Main.fibonacci(10);
+        for ( int i = 0 ; i < a.length ; i++ ) {
+            System.out.println(a[i]);
+        }
+
+        Arrays.asList(a).forEach(System.out::println);
     }
 
     private static int factorial(int num){
@@ -19,6 +26,35 @@ public class Main {
 
     private static int factorialStream(int num){
         int result = IntStream.rangeClosed(1, num).reduce(1, ( (x, y) -> x * y ));
+        return result;
+    }
+
+
+    public static void calculateFactorial(int number){
+        BigInteger one = BigInteger.ONE;
+
+        for ( int i = 1 ; i <= number ; i++ ) {
+            BigInteger integer = BigInteger.valueOf(i);
+            one = one.multiply(integer);
+        }
+        System.out.println(one);
+    }
+
+
+    private static int[] fibonacci(int a){
+
+
+        int[] result = new int[a];
+        int number1 = 0;
+        int number2 = 1;
+        int sum;
+
+        for ( int i = 1 ; i < a ; ++i ) {
+            sum = number1 + number2;
+            result[i] = sum;
+            number1 = number2;
+            number2 = sum;
+        }
         return result;
     }
 }
